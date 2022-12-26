ad2b3f309cda18b54821bbbf7534f18b56a651b2
Kota Suzuki <s21c1064cl@s.chibakoudai.jp>
2022-12-26 11:29:25 +0900

    edited mypkg

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2022 Kota Suzuki s21c1064cl@s.chibakoudai.jp
+# SPDX-License Identifier: BSD-3-Clause
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
